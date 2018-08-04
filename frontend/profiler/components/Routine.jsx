ca601361734d79a8cd72751bb8e6956583882745
Timo Paulssen <timonator@perpetuum-immobile.de>
2018-08-04 02:23:46 +0200

    turn Routine into class component so can have state
    so the user can change between tabs, like "callees",
    "paths", "callers", maybe "allocations", ...

--- frontend/profiler/components/Routine.jsx
+++ frontend/profiler/components/Routine.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Container, Button, Table } from 'reactstrap';
 import RoutineList from "./RoutineList";
 
@@ -34,107 +34,162 @@ export function timeToHuman(time, suffix = 'ms', smaller) {
     }
 }
 
-export default function Routine({ routine, metadata, columns, expanded, allRoutineChildren, onExpandButtonClicked, maxTime }) {
-  if (typeof columns === "string") {
-      columns = columns.split(" ");
-  }
-  const myMetadata = metadata[routine.id];
-  let columnFunctions = {
-      expand() {
-          return (<td key={"expand"}><Button onClick={() => onExpandButtonClicked(routine.id)}><i className="fas fa-folder-open"></i></Button></td>)
-      },
-      sitecount() {
-          return (<td key={"sitecount"}>{routine.sitecount}</td>)
-      },
-      nameInfo() {
-          return (<td key={"nameInfo"}>
-              <span className="routineName"><strong>{myMetadata.name}</strong></span><br />
-              <span className="routineFileInfo">{myMetadata.file}:{myMetadata.line}</span>
-          </td>)
-      },
-      entriesInfo() {
-          const jitText = (routine.jit_entries * 100 / routine.entries).toPrecision(3)
-          return (<td key={"entriesInfo"}>{numberFormatter(routine.entries)}
-              <div style={{float: "right"}}><small>
-                  {
-                      jitText != "0.00" ? jitText : "0"
-                  }% jit
-              </small></div>
-          </td>)
-      },
-      exclusiveInclusiveTime() {
-          var barWidthFirst, barWidthSecond, barWidthRest;
-          let willShowBar = typeof maxTime !== "undefined" && routine.exclusive_time <= routine.inclusive_time;
-          if (typeof maxTime !== "undefined") {
-              barWidthFirst  = (routine.exclusive_time / maxTime) * 100;
-              barWidthSecond = ((routine.inclusive_time - routine.exclusive_time) / maxTime) * 100;
-              barWidthRest   = (1 - routine.inclusive_time / maxTime) * 100;
-          }
-          const barStyle = {
-              height: "0.4em",
-              padding: "0px",
-              margin: "0px",
-              display: "inline-block"
-          }
-          return (
-              <td key={"exclusiveInclusiveTime"}>
-                  {timeToHuman(routine.exclusive_time, "ms", "ns")} / {timeToHuman(routine.inclusive_time, "ms", "ns")}<br />
-                  {
-                      routine.entries > 1 &&
-                      <small>
-                          {timeToHuman(routine.exclusive_time / routine.entries, "ms", "ns")} /
-                          {timeToHuman(routine.inclusive_time / routine.entries, "ms", "ns")}
-                          {" "}per entry
-                      </small>
-                  }
-                  {
-                      willShowBar && (
-                          <React.Fragment>
-                              <br />
-                              <div style={{position: "relative", width: "100%", bottom: "-0.3em", borderBottom: "1px solid grey"}}>
-                                <span style={{width: barWidthFirst + "%",  background: "darkblue", ...barStyle }}></span>
-                                <span style={{width: barWidthSecond + "%", background: "blue", ...barStyle}}></span>
-                                <span style={{width: barWidthRest + "%",   background: "lightgrey", ...barStyle}}></span>
-                              </div>
-                          </React.Fragment>
+export default class Routine extends Component<{ routine: *, metadata: *, columns: *, expanded: *, allRoutineChildren: *, onExpandButtonClicked: *, maxTime: *, parentEntries: * }> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tab: "callees",
+        }
+    }
 
-                      )
-                  }
-              </td>)
-      },
-      inlineInfo() {
-          const inlineText = (routine.inlined_entries * 100 / routine.entries).toPrecision(3)
-          return (
-              inlineText == "0.00"
-                  ? <td key={"inlineInfo"}>-</td>
-                  : <td key={"inlineInfo"}>{inlineText == "0.00" ? "0" : inlineText}% inlined</td>
-          )
-      }
-  };
-  return [
-    <tr key={routine.id + "_entry"}>
-      {
-        columns.map((name) => columnFunctions[name]())
-      }
-    </tr>,
-      expanded &&
-        <tr key={routine.id + "expanded"}>
-            <td colSpan={columns.length}>
-              <Container>
-                  {
-                      (!allRoutineChildren || typeof allRoutineChildren[routine.id] === "undefined")
-                      && <span>Loading, hold on ...</span>
-                  }
-                  {
-                      (allRoutineChildren && allRoutineChildren[routine.id])
-                      && <RoutineList routines={allRoutineChildren[routine.id]}
-                                      metadata={metadata}
-                                      columns={"sitecount nameInfo entriesInfo inlineInfo exclusiveInclusiveTime"}
-                                      maxTime={routine.inclusive_time}
-                      />
-                  }
-              </Container>
-            </td>
-        </tr>
-  ];
+    render() {
+        let {routine, metadata, columns, expanded, allRoutineChildren, onExpandButtonClicked, maxTime, parentEntries} = this.props;
+        if (typeof columns === "string") {
+            columns = columns.split(" ");
+        }
+        const myMetadata = metadata[routine.id];
+        let columnFunctions = {
+            expand() {
+                return (<td key={"expand"}>
+                    <Button onClick={() => onExpandButtonClicked(routine.id)}>
+                        <i className="fas fa-folder-open"/>
+                    </Button></td>
+                )
+            },
+            sitecount() {
+                return (<td key={"sitecount"}>{routine.sitecount}</td>)
+            },
+            nameInfo() {
+                return (<td key={"nameInfo"}>
+                    <span className="routineName"><strong>{myMetadata.name}</strong></span><br/>
+                    <span className="routineFileInfo">{myMetadata.file}:{myMetadata.line}</span>
+                </td>)
+            },
+            entriesInfo() {
+                const jitText = (routine.jit_entries * 100 / routine.entries).toPrecision(3)
+                return (<td key={"entriesInfo"}>
+                    <div style={{float: "right"}}>
+                        <small>
+                            {
+                                jitText !== "0.00" ? jitText : "0"
+                            }% jit
+                        </small>
+                    </div>
+                    <span>{numberFormatter(routine.entries)}</span>
+                    {
+                        typeof parentEntries !== "undefined"
+                            ? <React.Fragment><br/>
+                                <small>{numberFormatter(routine.entries / parentEntries, 2)} per entry</small>
+                            </React.Fragment>
+                            : null
+                    }
+                </td>)
+            },
+            exclusiveInclusiveTime() {
+                var barWidthFirst, barWidthSecond, barWidthRest;
+                let willShowBar = typeof maxTime !== "undefined" && routine.exclusive_time <= routine.inclusive_time;
+                if (typeof maxTime !== "undefined") {
+                    barWidthFirst = (routine.exclusive_time / maxTime) * 100;
+                    barWidthSecond = ((routine.inclusive_time - routine.exclusive_time) / maxTime) * 100;
+                    barWidthRest = (1 - routine.inclusive_time / maxTime) * 100;
+                }
+                const barStyle = {
+                    height: "0.4em",
+                    padding: "0px",
+                    margin: "0px",
+                    display: "inline-block"
+                }
+                return (
+                    <td key={"exclusiveInclusiveTime"}>
+                        {timeToHuman(routine.exclusive_time, "ms", "µs")} / {timeToHuman(routine.inclusive_time, "ms", "µs")}<br/>
+                        {
+                            routine.entries > 1 &&
+                            <small>
+                                {timeToHuman(routine.exclusive_time / routine.entries, "ms", "µs")} / {" "}
+                                {timeToHuman(routine.inclusive_time / routine.entries, "ms", "µs")}
+                                {" "}per entry
+                            </small>
+                        }
+                        {
+                            willShowBar && (
+                                <React.Fragment>
+                                    <br/>
+                                    <div style={{
+                                        position: "relative",
+                                        width: "100%",
+                                        bottom: "-0.3em",
+                                        borderBottom: "1px solid grey"
+                                    }}>
+                                        <span style={{
+                                            width: barWidthFirst + "%",
+                                            background: "darkblue", ...barStyle
+                                        }}/>
+                                        <span style={{
+                                            width: barWidthSecond + "%",
+                                            background: "blue", ...barStyle
+                                        }}/>
+                                        <span style={{
+                                            width: barWidthRest + "%",
+                                            background: "lightgrey", ...barStyle
+                                        }}/>
+                                    </div>
+                                </React.Fragment>
+
+                            )
+                        }
+                    </td>)
+            },
+            inlineInfo() {
+                const inlineText = (routine.inlined_entries * 100 / routine.entries).toPrecision(3)
+                return (
+                    inlineText === "0.00"
+                        ? <td key={"inlineInfo"}>-</td>
+                        : <td key={"inlineInfo"}>{inlineText}% inlined</td>
+                )
+            }
+        };
+        let expandedComponent = <React.Fragment/>;
+        if (expanded) {
+            if (this.state.tab === "callees") {
+                expandedComponent = (<Container>
+                    {
+                        (!allRoutineChildren || typeof allRoutineChildren[routine.id] === "undefined")
+                        && <span>Loading, hold on ...</span>
+                    }
+                    {
+                        (allRoutineChildren && allRoutineChildren[routine.id])
+                        && <RoutineList routines={allRoutineChildren[routine.id]}
+                                        metadata={metadata}
+                                        columns={"sitecount nameInfo entriesInfo inlineInfo exclusiveInclusiveTime"}
+                                        maxTime={routine.inclusive_time}
+                                        parentEntries={routine.entries}
+                        />
+                    }
+                </Container>);
+            }
+            else if (this.state.tab === "paths") {
+                if (this.state.paths.loading) {
+                    expandedComponent = (<Container>
+                        <div>Hold on ...</div>
+                    </Container>);
+                }
+                else {
+
+                }
+            }
+        }
+        return [
+            <tr key={routine.id + "_entry"}>
+                {
+                    columns.map((name) => columnFunctions[name]())
+                }
+            </tr>,
+            expanded &&
+            <tr key={routine.id + "expanded"}>
+                <td colSpan={columns.length}>
+                    { expandedComponent }
+                </td>
+            </tr>
+        ];
+    }
 }
