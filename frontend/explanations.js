3e40a31ad9c4dfcde5dc17cb15a4a186b7645fbf
Timo Paulssen <timonator@perpetuum-immobile.de>
2018-11-06 20:17:11 +0100

    add more repr/type explanations
    not that they are exposed to the user yet, or anything

--- frontend/explanations.js
+++ frontend/explanations.js
@@ -4,5 +4,10 @@ export const explanations = {
     reprs: {
         NativeRef: "Temporary objects used to make native lexicals, locals, attributes, and positional values usable like a read-write Scalar.",
         VMHash: "Representation for objects that have key-value access using a hash table.",
+        MVMContext: "Objects used to hold on to lexical scopes for later traversal.",
+        ConcBlockingQueue: "Objects that implement a queue that blocks when no value is present while trying to receive one.",
+    },
+    types: {
+        BOOTCode: `This is the type for closures. Whenever a routine or block gets returned from a function, it holds on to data from that function and outer scopes. These are represented in a BOOTCode object.`,
     }
 }
