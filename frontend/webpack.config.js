8584ced7489bf56897caa30562e8bbac585da27e
Timo Paulssen <timonator@perpetuum-immobile.de>
2019-09-28 23:14:35 +0200

    whoops, accidentally had all these files deleted

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: [
+    "core-js/modules/es6.promise",
+    "core-js/modules/es6.array.iterator",
+    './index.js'
+  ],
+  context: path.resolve(__dirname),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        resolve: { extensions: [".js", ".jsx"] },
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devtool: "eval-source-map",
+  watch: true,
+    output: {
+        path: path.resolve(__dirname, '../static/js/'),
+        publicPath: "/js/"
+    }
+};
