bb95694697eff633dd1d7f3a66ff33ad2d6cd9e5
Timo Paulssen <timonator@perpetuum-immobile.de>
2020-09-18 22:55:29 +0200

    first naive attempt to appimage
    i have no idea what i'm doing

---
+++ appimage/do_stuff.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+########################################################################
+# Package the binaries built on Travis-CI as an AppImage
+# By Simon Peter 2016
+# For more information, see http://appimage.org/
+########################################################################
+
+set -e
+
+export ARCH="$(arch)"
+export VERSION="v0.0.1"
+
+APP=moarperf
+LOWERAPP=${APP,,}
+
+mkdir -p "$HOME/$APP/$APP.AppDir/usr/"
+BUILD_PATH="$(pwd)"
+
+git clone https://github.com/rakudo/rakudo
+cd rakudo
+git checkout 2020.08.02
+perl Configure.pl --prefix=/usr/rakudo/install/ --gen-moar
+make -j2 install
+
+cd ../
+
+git clone https://github.com/ugexe/zef
+
+cd zef
+/usr/rakudo/install/bin/raku -I . bin/zef install .
+cd $BUILD_PATH
+
+/usr/rakudo/install/bin/zef install --/test \
+    "JSON::Fast" \
+    "OO::Monitors" \
+    "Cro::HTTP" \
+    "Cro::WebSocket" \
+    "App::MoarVM::HeapAnalyzer" \
+    "Digest::SHA1::Native" \
+    "DBIish"
+
+/usr/rakudo/install/bin/zef install .
+
+cd "$HOME/$APP/"
+
+wget -q https://github.com/probonopd/AppImages/raw/master/functions.sh -O ./functions.sh
+. ./functions.sh
+
+cd $APP.AppDir
+
+cat >> AppRun <<"EOF"
+#!/bin/sh
+./install/bin/raku ./rakuapp/service.p6
+EOF
+
+mkdir rakuapp
+cp    $BUILD_PATH/META6.json rakuapp
+cp    $BUILD_PATH/service.p6 rakuapp
+cp -r $BUILD_PATH/static rakuapp
+cp -r $BUILD_PATH/lib rakuapp
+cp -r "/usr/rakudo/install/" .
+
+########################################################################
+# Copy desktop and icon file to AppDir for AppRun to pick them up
+########################################################################
+
+#cp "${BUILD_PATH}/appimage/${LOWERAPP}.desktop" .
+#cp "${BUILD_PATH}/appimage/${LOWERAPP}.png" .
+
+########################################################################
+# Copy in the dependencies that cannot be assumed to be available
+# on all target systems
+########################################################################
+
+copy_deps
+
+
+########################################################################
+# Patch away absolute paths; it would be nice if they were relative
+########################################################################
+
+find . -type f -exec sed -i -e 's|/usr|././|g' {} \;
+find . -type f -exec sed -i -e 's@././/bin/env@/usr/bin/env@g' {} \;
+
+########################################################################
+# AppDir complete
+# Now packaging it as an AppImage
+########################################################################
+
+cd .. # Go out of AppImage
+
+mkdir -p ../out/
+generate_type2_appimage
